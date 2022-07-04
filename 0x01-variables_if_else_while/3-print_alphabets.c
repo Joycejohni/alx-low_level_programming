bb8313b435e6905d9e5f3ac924e7922a3b92da1a
joyce john <janellejohn40@gmail.com>
2022-07-04 13:23:44 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase, and
+ ** main - prints the alphabet in lowercase, and
  * then in uppercase, followed by a new line.
  * Return: 0 if successful
  * otherwise return a non-zero
@@ -25,4 +25,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-
+}
