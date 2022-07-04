f84714efb09dd33a23072ba03fbe5be0d4c94ae5
joyce john <janellejohn40@gmail.com>
2022-07-04 06:46:52 -0700

    modified

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,13 +10,13 @@ int main(void)
 	char charType;
 	int intType;
 	long int longintType;
-	long long longType;
+	long long int longType;
 	float floatType;
 
 	printf("Size of a char: %lu bytes\n", sizeof(charType));
 	printf("Size of an int: %lu bytes\n", sizeof(intType));
 	printf("Size of a long int: %lu bytes\n", sizeof(longintType));
-	printf("Size of a long long: %lu bytes\n", sizeof(longType));
+	printf("Size of a long long int: %lu bytes\n", sizeof(longType));
 	printf("Size of a float: %lu bytes\n", sizeof(floatType));
 	return (0);
 }
